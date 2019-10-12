5663b4232aeec61f20f8deadb66716a28ae7ec73
Giuseppe Capezzuto <capezzutog@gmail.com>
2019-10-12 17:53:07 +0200

    Refactored class GildedRose.java - Refactored main loop by replacing the existing loop with a for-each loop - Added constants to avoid code duplication - Added methods to increase and decrease the quality of a good

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,63 +1,80 @@
 package com.gildedrose;
 
 class GildedRose {
+
     Item[] items;
+
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_TAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String AGED_BRIE = "Aged Brie";
+    private static final int MINIMUM_QUALITY_VALUE = 0;
+    private static final int MAX_QUALITY_VALUE = 50;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        //
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            performUpdate(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+    private void performUpdate(Item item) {
+        if (!item.name.equals(AGED_BRIE)
+                && !item.name.equals(BACKSTAGE_TAFKAL80ETC)) {
+            if (item.quality > MINIMUM_QUALITY_VALUE) {
+                if (!item.name.equals(SULFURAS)) {
+                    decrementQuality(item);
                 }
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+        } else {
+            if (item.quality < MAX_QUALITY_VALUE) {
+                incrementQuality(item);
+                
+                if (item.name.equals(BACKSTAGE_TAFKAL80ETC)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < MAX_QUALITY_VALUE) {
+                            incrementQuality(item);
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    
+                    if (item.sellIn < 6) {
+                        if (item.quality < MAX_QUALITY_VALUE) {
+                            incrementQuality(item);
+                        }
                     }
                 }
             }
         }
+        
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+        
+        if (item.sellIn < 0) {
+            if (!item.name.equals(AGED_BRIE)) {
+                if (!item.name.equals(BACKSTAGE_TAFKAL80ETC)) {
+                    if (item.quality > MINIMUM_QUALITY_VALUE) {
+                        if (!item.name.equals(SULFURAS)) {
+                            decrementQuality(item);
+                        }
+                    }
+                } else {
+                    item.quality = MINIMUM_QUALITY_VALUE;
+                }
+            } else {
+                if (item.quality < MAX_QUALITY_VALUE) {
+                    incrementQuality(item);
+                }
+            }
+        }
+    }
+    private void incrementQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+
+    private void decrementQuality(Item item) {
+        item.quality = item.quality - 1;
     }
 }
